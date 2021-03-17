141ff290fca6c10c14d57fc442132feb2deeb31f
Gabriela Sígolo <69328848+gabrielasigolo@users.noreply.github.com>
2021-03-17 11:09:14 -0300

    Update exercicioVetor17-03.c

--- exercicioVetor17-03.c
+++ exercicioVetor17-03.c
@@ -1,46 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
-#include <string.h>
 
-int main () {
-	setlocale(LC_ALL, "Portuguese");
+int main()
+{				//		0   1   2    3   4 					//       0   1  2  3  4 
+	char gabarito[5]= {'a','a','a', 'b','c'} ,  respostaAluno[5]; // 'a' 'b'
+	   // 10 linhas x 25 colunas  -- matriz tem l e c
+	char nome[3] [25]; 
+	int i, acertos=0, nota[3], aluno;
+	setlocale(LC_ALL, "portuguese");
 	
-	char questoes[5] = {'a', 'c', 'd', 'b', 'e'}, resposta[5], nome [10] [25];
-	int i, pontos, pontuacao, aluno, nota[3];  
-	
-for(aluno=0; aluno < 3; aluno++) 
+for (aluno =0; aluno< 3; aluno++) //controla a entrada do aluno
 {
-	
 	system("cls");
-	puts("Nome do aluno: "); fflush(stdin);
-	gets(nome[aluno]); // recebe o nome completo com espaço 
+	puts("Nome do aluno...: "); 
+	fflush(stdin); gets(nome[aluno]);
 	
-	pontos=0;
-	printf("--- QUESTÕES ---");
-	for(i=0; i < 5; i++)
-	{
-		printf("\nQuestão %iº --- Resposta: ", i+1);
-		scanf(" %c", &resposta[i]);	
-
-	if(questoes[i] == resposta[i]) 
-	{	
-		pontos++;
-		printf("Acertou");
-	} 
-	else 
-	{
-		printf("Errou");
-	}
-
-}
-	
-	pontuacao = pontos * 2;
-
-	printf("\n %s, sua nota final foi: %i \n\n", nome, pontuacao);
+	acertos=0;	
+		for (i= 0; i< 5; i++) //controla as respostas do aluno
+		{
+			printf("\nQual a sua resposta a %ia questão? ", i+1);
+			scanf(" %c", &respostaAluno[i]);
+			if (respostaAluno[i] == gabarito[i])
+			{
+			   acertos++;
+			   printf("\nAcertou %i questão ", acertos);
+			}
+			else
+				printf("\nErrou a questão");
+		}
+		//fim da verificação das respostas do aluno
+	nota[aluno] = acertos * 2;
+	printf("\nA nota do %s é %i \n\n", nome, nota[aluno]);
 	system("pause");
+} //fim da entrada
+
+printf("\n\n Relatório das notas");
+for (aluno=0; aluno<3; aluno++)
+{
+	printf("\n Aluno %s teve Nota %i ", nome[aluno], nota[aluno]);
 }
 
-	printf("\n Os nomes dos alunos %s %s %s", nome[0], nome[1], nome[2]);
-
-	return 0; 
+ printf("\nFim da correção da prova\n\n");
+ system("pause");	
+ return 0;	
 }
